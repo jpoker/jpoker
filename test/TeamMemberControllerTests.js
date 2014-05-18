0b8583db09b2f823c15b7a687a5386098b7f199a
Vic Trouble <vic.trouble@gmail.com>
2014-05-18 21:16:30 +0300

    after code review: introduced test fixture

--- test/TeamMemberControllerTests.js
+++ test/TeamMemberControllerTests.js
@@ -4,32 +4,34 @@ var TeamMemberController = require('../server/TeamMemberController.js').TeamMemb
 
 describe('TeamMemberController', function() {
 
+    beforeEach(function() {
+        teamMember = new TeamMember();
+        controller = new TeamMemberController(teamMember);
+    });
+
+    afterEach(function() {
+        controller = null;
+        teamMember = null;
+    });
+
     it('should change estimate to given when provided', function() {
-        var teamMember = new TeamMember();
-        var controller = new TeamMemberController(teamMember);
         controller.provideEstimate(13);
         assert.equal(13, teamMember.exposedCard);
     });
 
     it('should remove estimate when recalled', function() {
-        var teamMember = new TeamMember();
-        var controller = new TeamMemberController(teamMember);
         controller.provideEstimate(13);
         controller.recallEstimate();
         assert.equal(null, teamMember.exposedCard);
     });
 
     it('should throw when empty card provided', function() {
-        var teamMember = new TeamMember();
-        var controller = new TeamMemberController(teamMember);
         assert.throws(function() {
             controller.provideEstimate(null);
             });
     });
 
     it('should throw when estimate is already provided', function() {
-        var teamMember = new TeamMember();
-        var controller = new TeamMemberController(teamMember);
         controller.provideEstimate(13);
         assert.throws(function() {
             controller.provideEstimate(14);
