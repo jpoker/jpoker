3acde639ef2dfdae6d85c6e84092909459286e5d
Vic Trouble <vic.trouble@gmail.com>
2014-06-01 21:59:13 +0300

    correct error handling

--- try-mongo.js
+++ try-mongo.js
@@ -1,13 +1,24 @@
-try
+function db_err()
 {
-    var mongoose = require('mongoose');
-    var db = mongoose.connect('mongodb://localhost/test');
+    console.log('connection error!');
+}
 
+function db_open()
+{
     var userSchema = new mongoose.Schema({name: String, age: Number});
     var User = mongoose.model('User', userSchema);
 
     console.log(User.find({name: 'Vasya'}));
     console.log('finished');
+}
+
+try
+{
+    var mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/test');
+    var db = mongoose.connection;
+    db.on('error', db_err);
+    db.once('open', db_open);
 }
 catch (err)
 {
