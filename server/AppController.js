cf738fec117dad2f4367ebe9d6b8229b7a313a49
unknown <shulgin@KIEMLAPVSHUL2.matone.materialise>
2014-06-08 16:06:38 +0300

    renamed RootController into AppController

--- server/AppController.js
+++ server/AppController.js
@@ -4,17 +4,17 @@
 
 var Session = require('./Session.js').Session;
 
-function RootController(db) {
+function AppController(db) {
     this.db = db;
 }
 
-RootController.prototype.createSession = function(scrumMasterName) {
+AppController.prototype.createSession = function(scrumMasterName) {
         if (scrumMasterName === '')
             throw new Error('not implemented!');
 
         return this.db.createSession(scrumMasterName);
 };
 
-exports.RootController = RootController;
+exports.AppController = AppController;
 
 }());
--- test/AppControllerTests.js
+++ test/AppControllerTests.js
@@ -1,20 +1,20 @@
 var assert = require('assert');
-var RootController = require('../server/RootController.js').RootController;
+var AppController = require('../server/AppController.js').AppController;
 var FakeDB = require('../server/FakeDB.js').FakeDB;
 
-describe('RootController', function() {
+describe('AppController', function() {
 
     it('createSession should throw when scrum master name is empty', function() {
         assert.throws(function() {
             var db = new FakeDB();
-            var controller = new RootController(db);
+            var controller = new AppController(db);
             controller.createSession('');
             }, Error);
     });
 
     it('createSession should return session with given scrum master name', function() {
         var db = new FakeDB();
-        var controller = new RootController(db);
+        var controller = new AppController(db);
         session = controller.createSession('Vasya Pupkin');
         assert.equal('Vasya Pupkin', session.scrumMasterName);
     });
