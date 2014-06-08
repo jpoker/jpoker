f630a213352cdf0295364e337100e104d421539e
Vic Trouble <vic.trouble@gmail.com>
2014-06-08 18:03:07 +0300

    make SessionController work via DB

--- test/SelectionTests.js
+++ test/SelectionTests.js
@@ -1,4 +1,6 @@
 var assert = require('assert');
+var AppController = require('../server/AppController.js').AppController;
+var FakeDB = require('../server/FakeDB.js').FakeDB;
 var Card = require('../server/Card.js').Card;
 var Exposition = require('../server/Card.js').Exposition;
 var Session = require('../server/Session.js').Session;
@@ -16,16 +18,18 @@ describe('Selection', function () {
     });
 
     it('afterUserSelectsCardExpositionShouldBeEqual', function () {
-        var session = new Session();
+        var db = new FakeDB();
+        var appController = new AppController(db);
+        var session = appController.createSession();
+
         var deck = [new Card('fibonacci_0', '')
                   , new Card('fibonacci_1', '')
                   , new Card('fibonacci_2', '')
                   , new Card('unsure', '')
                   , new Card('coffee_break', '')];
-
         session.setDeck(deck);
 
-        var sessionController = new SessionController(session);
+        var sessionController = new SessionController(session, db);
         var team_member = sessionController.joinSession('Jon');
         var teamMemberController = new TeamMemberController(team_member);
         var exposedCard = session.getCardByName('coffee_break');
--- test/SessionControllerTests.js
+++ test/SessionControllerTests.js
@@ -1,20 +1,25 @@
 var assert = require('assert');
+var AppController = require('../server/AppController.js').AppController;
 var Session = require('../server/Session.js').Session;
 var SessionController = require('../server/SessionController.js').SessionController;
+var FakeDB = require('../server/FakeDB.js').FakeDB;
 
 describe('SessionController', function() {
 
+    beforeEach(function() {
+        var db = new FakeDB();
+        var appController = new AppController(db);
+        var session = appController.createSession();
+        controller = new SessionController(session, db);
+    });
+
     it('joinSession should throw when team member name is empty', function() {
         assert.throws(function() {
-            var session = new Session();
-            var controller = new SessionController(session);
             controller.joinSession("");
             }, Error);
     });
 
     it('joinSession should return team member with given name', function() {
-        var session = new Session();
-        var controller = new SessionController(session);
         var teamMember = controller.joinSession('Petya Pupkin');
         assert.equal('Petya Pupkin', teamMember.name);
     });
