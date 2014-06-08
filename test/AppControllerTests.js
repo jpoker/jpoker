438533799f442e935f235a770c928dd6f7e1a59f
unknown <shulgin@KIEMLAPVSHUL2.matone.materialise>
2014-06-08 16:12:06 +0300

    added fixtures

--- test/AppControllerTests.js
+++ test/AppControllerTests.js
@@ -4,17 +4,18 @@ var FakeDB = require('../server/FakeDB.js').FakeDB;
 
 describe('AppController', function() {
 
+    beforeEach(function() {
+        var db = new FakeDB();
+        controller = new AppController(db);
+    });
+
     it('createSession should throw when scrum master name is empty', function() {
         assert.throws(function() {
-            var db = new FakeDB();
-            var controller = new AppController(db);
             controller.createSession('');
             }, Error);
     });
 
     it('createSession should return session with given scrum master name', function() {
-        var db = new FakeDB();
-        var controller = new AppController(db);
         session = controller.createSession('Vasya Pupkin');
         assert.equal('Vasya Pupkin', session.scrumMasterName);
     });
