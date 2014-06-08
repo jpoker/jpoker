e4860cdf69c48bb5d4ab583ee14c29fd04d91dd5
unknown <shulgin@KIEMLAPVSHUL2.matone.materialise>
2014-06-08 16:03:39 +0300

    make RootController work through the DB

--- server/RootController.js
+++ server/RootController.js
@@ -4,14 +4,15 @@
 
 var Session = require('./Session.js').Session;
 
-function RootController() {
+function RootController(db) {
+    this.db = db;
 }
 
 RootController.prototype.createSession = function(scrumMasterName) {
         if (scrumMasterName === '')
             throw new Error('not implemented!');
 
-        return new Session(scrumMasterName);
+        return this.db.createSession(scrumMasterName);
 };
 
 exports.RootController = RootController;
--- test/RootControllerTests.js
+++ test/RootControllerTests.js
@@ -1,17 +1,20 @@
 var assert = require('assert');
 var RootController = require('../server/RootController.js').RootController;
+var FakeDB = require('../server/FakeDB.js').FakeDB;
 
 describe('RootController', function() {
 
     it('createSession should throw when scrum master name is empty', function() {
         assert.throws(function() {
-            var controller = new RootController();
-            controller.createSession("");
+            var db = new FakeDB();
+            var controller = new RootController(db);
+            controller.createSession('');
             }, Error);
     });
 
     it('createSession should return session with given scrum master name', function() {
-        var controller = new RootController();
+        var db = new FakeDB();
+        var controller = new RootController(db);
         session = controller.createSession('Vasya Pupkin');
         assert.equal('Vasya Pupkin', session.scrumMasterName);
     });
