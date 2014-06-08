d5b75968d7734ad8278b1898d43e0c585996b357
unknown <shulgin@KIEMLAPVSHUL2.matone.materialise>
2014-06-08 16:24:28 +0300

    added appController in the application script; specified application.js for run via npm start

--- server/application.js
+++ server/application.js
@@ -3,6 +3,12 @@ var morgan = require('morgan');
 var server = express();
 var PORT = 9372;
 
+var AppController = require('./AppController.js').AppController;
+var FakeDB = require('./FakeDB.js').FakeDB;
+
+var db = new FakeDB();
+var appController = new AppController(db); // use it in routes
+
 server.use(morgan('short'));
 server.use(express.static(__dirname + './../client'));
 
