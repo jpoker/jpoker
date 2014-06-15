321327b68293b4b7cf98e4312533b2e242c44ac3
s.shynkaruk <Sergiy.shynkaruk@gmail.com>
2014-06-15 21:36:10 +0300

    1) migration to VS11.0 2) added middleware dependency 3) post session creation synch. TODO: asynch. creation

--- server/application.js
+++ server/application.js
@@ -14,7 +14,22 @@ var appController = new AppController(db);
 server.use(morgan('short'));
 server.use('/static', express.static(__dirname + './../client/static'));
 
+// parse urlencoded request bodies into req.body
+var bodyParser = require('body-parser')
+server.use(bodyParser.urlencoded())
+server.use(bodyParser.json());
 
+server.use(express.static(__dirname + '/public'));
+
+// This route receives the posted form.
+// As explained above, usage of 'body-parser' means
+// that `req.body` will be filled in with the form elements
+server.post('/', function(req, res){
+  var userName = req.body.user;
+  var html = 'Hello: ' + userName + '.<br>' +
+             '<a href="/">Try again.</a>';
+  res.send(html);
+});
 
 //root of the website
 server.get('/', function(req, res) {
@@ -22,8 +37,8 @@ server.get('/', function(req, res) {
 });
 
 server.post('/sessions/new/:master_id', function (req, res) {
-    res.send('session created! ' +
-        appController.createSession(req.params.master_id).id);
+    appController.createSession(req.params.master_id);
+    res.redirect('/static/session.html');
 });
 
 server.post('/sessions/edit/:session_id/user/:user_id', function (req, res) {
@@ -35,8 +50,6 @@ server.post('/sessions/edit/:session_id/user/:user_id', function (req, res) {
 });
 
 server.get('/session/:id/users/:info', function (req, res) {
-
-
     res.send('connected users ' + req.params.info);
 });
 
