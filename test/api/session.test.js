9ed4f35f37fca9b283a03ba86f2dece9bf15f30f
Vic Trouble <vic.trouble@gmail.com>
2014-07-06 15:37:42 +0300

    API tests template

---
+++ test/api/session.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var assert = require('chai').assert;
+
+describe('session api', function () {
+
+    before(function () {
+    });
+
+    after(function () {
+    });
+
+    it('dummy', function () {
+        assert.equal(4, 2 * 2);
+    });
+
+});
