908ea949759a70f6730d95331e69fe88bb36aac1
Vic Trouble <vic.trouble@gmail.com>
2014-07-10 22:52:48 +0300

    it('should create session on POST /sessions')

--- test/api/session.test.js
+++ test/api/session.test.js
@@ -1,17 +1,17 @@
 'use strict';
 
 var assert = require('chai').assert;
+var app = require('../../server/application.js')
+var request = require('supertest');
 
 describe('session api', function () {
 
-    before(function () {
-    });
-
-    after(function () {
-    });
-
-    it('dummy', function () {
-        assert.equal(4, 2 * 2);
+    it('should create session on POST /sessions', function (done) {
+        request(app)
+            .post('/sessions/new/Vasya')
+//            .send({name: 'Master'})
+            .expect(200)
+            .expect('session created! 1', done);
     });
 
 });
